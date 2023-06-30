4d126f491e3c2ef684a2a851a713f257a9f4acac
Bildi Grafiks <138207173+bildigrafiks@users.noreply.github.com>
2023-06-30 12:10:54 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -91,7 +91,6 @@ function setup() {
 
   let d = new Date();
 
-  document.getElementById("insertDate").innerHTML = `Ultima actualización: ${d.getDate()}.${d.getMonth()}.${d.getFullYear()}`;
   frameRate(30);
   colorMode(RGB);
 
